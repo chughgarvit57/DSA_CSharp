9a03cf7ed00464febc9fc5c557dfaed2e5e89e45
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-28 00:09:05 +0530

    Palindrome Check Program

---
+++ ProblemSolving/PalindromeProgram.cs
@@ -0,0 +1,23 @@
+﻿namespace ProblemSolving
+{
+    internal class PalindromeProgram
+    {
+        public static void PalindromeCheck(string str)
+        {
+            string copy_str = str;
+            string reversedStr = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversedStr += str[i];
+            }
+            if (copy_str.Equals(reversedStr,StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine($"\"{str}\" is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"\"{str}\" is not a palindrome.");
+            }
+        }
+    }
+}
