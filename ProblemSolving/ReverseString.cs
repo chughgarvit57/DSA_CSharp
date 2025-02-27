d03d7faed82bfc5905a60d8c611170e075dd4d37
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-28 00:07:35 +0530

    Reverse A String Program

---
+++ ProblemSolving/ReverseString.cs
@@ -0,0 +1,19 @@
+﻿namespace ProblemSolving
+{
+    internal class ReverseString
+    {
+        public static void ReverseAString(string str)
+        {
+            /*for (int i = str.Length - 1; i >= 0; i--)
+            {
+                Console.Write(str[i]);
+            }*/
+            string reversedStr = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversedStr += str[i];
+            }
+            Console.Write(reversedStr);
+        }
+    }
+}
