baca6e7c056bfdfcaa2304c378c57bc4223f014c
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-28 22:41:06 +0530

    Missing Number Program

---
+++ ProblemSolving/MissingNumber.cs
@@ -0,0 +1,18 @@
+﻿namespace ProblemSolving
+{
+    internal class MissingNumber
+    {
+        public static void FindMissingNumber(int[] arr)
+        {
+            int num = arr.Length + 1;
+            int total = num * (num + 1) / 2;
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            int missingNumber = total - sum;
+            Console.WriteLine($"Missing number in the array is: {missingNumber}");
+        }
+    }
+}
