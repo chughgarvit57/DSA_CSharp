64ad4729d3211f1e90c273543bc864c6fde96ec8
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-28 15:58:49 +0530

    String Compression Program

---
+++ ProblemSolving/StringCompressionProgram.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace ProblemSolving
+{
+    internal class StringCompressionProgram
+    {
+        public static void CompressString(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                Console.WriteLine("String is empty.");
+                return;
+            }
+            string compressedString = "";
+            int count = 1;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i < str.Length - 1 && str[i] == str[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    compressedString += str[i] + count.ToString();
+                    count = 1;
+                }
+            }
+            Console.WriteLine($"Compressed String: {compressedString}");
+        }
+    }
+}
