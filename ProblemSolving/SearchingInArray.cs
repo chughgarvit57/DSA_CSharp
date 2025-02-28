3623966004fb9d446dbb5db63c9af6a794aa2c95
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-28 22:31:57 +0530

    Linear Search Program

---
+++ ProblemSolving/SearchingInArray.cs
@@ -0,0 +1,18 @@
+﻿namespace ProblemSolving
+{
+    internal class SearchingInArray
+    {
+        public static void SearchInArray(int[] arr, int key)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == key)
+                {
+                    Console.WriteLine($"{key} found at index: " + i);
+                    return;
+                }
+            }
+            Console.WriteLine($"{key} not found in the array.");
+        }
+    }
+}
