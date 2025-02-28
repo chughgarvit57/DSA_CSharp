b1b46a6b5378f4d3dd1af5c875695ed3ca82b574
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-28 20:51:15 +0530

    menu-driven-program code pushed on strings problems

--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -4,7 +4,61 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is the dev branch. Check different files/branches for different problems.");
+            Console.WriteLine("Press buttons for seeing the solutions to the problems given below:");
+            while (true)
+            {
+                Console.WriteLine("Press 1 for ---------------> Reverse A String Program");
+                Console.WriteLine("Press 2 for ---------------> Palindrome Check Program");
+                Console.WriteLine("Press 3 for ---------------> Anagram Check Program");
+                Console.WriteLine("Press 4 for ---------------> Count Vovwels & Consonants Program");
+                Console.WriteLine("Press 5 for ---------------> String Compression Program");
+                Console.WriteLine("Press 6 for ---------------> Find the First Non-Repeating Character Program\n");
+                Console.WriteLine("Press any button:");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter a string:");
+                        string str = Console.ReadLine();
+                        ReverseString.ReverseAString(str);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter a string:");
+                        string str2 = Console.ReadLine();
+                        PalindromeProgram.PalindromeCheck(str2);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the first string:");
+                        string str3 = Console.ReadLine();
+                        Console.WriteLine("Enter the second string:");
+                        string str4 = Console.ReadLine();
+                        AnagramProgram.AreAnagrams(str3, str4);
+                        break;
+                    case 4:
+                        CountVowelsAndConsonants.CountVowelsAndConsonantsProgram();
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter a string:");
+                        string str6 = Console.ReadLine();
+                        StringCompressionProgram.CompressString(str6);
+                        break;
+                    case 6:
+                        Console.WriteLine("Enter a string:");
+                        string str7 = Console.ReadLine();
+                        NonRepeatingCharacter.FirstNonRepeatingCharacterProgram(str7);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+                Console.WriteLine("\nDo you wish to continue(y/n)?");
+                char confirm = Convert.ToChar(Console.ReadLine());
+                if(confirm == 'n')
+                {
+                    Console.WriteLine("Exiting...\nGoodBye....!");
+                    break;
+                }
+            }
         }
     }
 }
