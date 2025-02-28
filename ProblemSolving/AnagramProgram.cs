c2bdc1f8e0b3b8e5cb417735c8b8615cdb707709
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-28 11:37:33 +0530

    Anagram Check Program

---
+++ ProblemSolving/AnagramProgram.cs
@@ -0,0 +1,33 @@
+﻿namespace ProblemSolving
+{
+    internal class AnagramProgram
+    {
+        public static void AreAnagrams(string str1, string str2)
+        {
+            bool isAnagram = true;
+            if (str1.Length != str2.Length)
+            {
+                isAnagram = false;
+            }
+            List<char> str1Chars = new List<char>(str1.ToLower().ToCharArray());
+            List<char> str2Chars = new List<char>(str2.ToLower().ToCharArray());
+            str1Chars.Sort();
+            str2Chars.Sort();
+            for(int i = 0; i < str1Chars.Count; i++)
+            {
+                if (str1Chars[i] != str2Chars[i])
+                {
+                    isAnagram = false;
+                }
+            }
+            if(isAnagram)
+            {
+                Console.WriteLine($"{str1} and {str2} are anagrams.");
+            }
+            else
+            {
+                Console.WriteLine($"{str1} and {str2} are not anagrams.");
+            }
+        }
+    }
+}
