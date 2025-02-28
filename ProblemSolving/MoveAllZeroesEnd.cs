1ab9db82f4615db50798cc8386e8521cbed26a6d
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-28 23:07:50 +0530

    Move All Zeroes In End Program

---
+++ ProblemSolving/MoveAllZeroesEnd.cs
@@ -0,0 +1,28 @@
+﻿namespace ProblemSolving
+{
+    internal class MoveAllZeroesEnd
+    {
+        public static void MoveAllZeroesInEnd(int[] arr)
+        {
+            // Ex Arr - [0, 1, 0, 3, 12]
+            // Ex O/P - [1, 3, 12, 0, 0]
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != 0)
+                {
+                    arr[count++] = arr[i];
+                }
+            }
+            while (count < arr.Length)
+            {
+                arr[count++] = 0;
+            }
+            Console.WriteLine("Array after moving zeroes in end:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
