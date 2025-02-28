5749aa5b999838f16c5fa77dee269a673c87d856
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-28 20:07:27 +0530

    First Non Repeating Character Program

---
+++ ProblemSolving/NonRepeatingCharacter.cs
@@ -0,0 +1,28 @@
+﻿namespace ProblemSolving
+{
+    internal class NonRepeatingCharacter
+    {
+        public static void FirstNonRepeatingCharacterProgram(string str)
+        {
+            for(int i = 0; i < str.Length; i++)
+            {
+                char currentChar = str[i];
+                bool isUnique = true;
+                for(int j = 0; j < str.Length; j++)
+                {
+                    if (i != j && str[i] == str[j])
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+                if (isUnique)
+                {
+                    Console.WriteLine($"First Non-Repeating Character: {currentChar}");
+                    return;
+                }
+            }
+            Console.WriteLine("No non-repeating character found.");
+        }
+    }
+}
