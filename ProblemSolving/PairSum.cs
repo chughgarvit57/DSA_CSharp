6511a6733a8ed4dd392cf34e2833f4fd194ecfa5
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-28 22:09:18 +0530

    Corrected code for pair sum program

--- ProblemSolving/PairSum.cs
+++ ProblemSolving/PairSum.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProblemSolving
+﻿namespace ProblemSolving
 {
     internal class PairSum
     {
+        public static void CalculatePairSum(int[] arr, int sum)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[i] + arr[j] == sum)
+                    {
+                        Console.WriteLine("[" + arr[i] + ", " + arr[j] + "]");
+                    }
+                }
+            }
+        }
     }
 }
