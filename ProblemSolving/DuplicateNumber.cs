e65b207084b31a854588c636baf6db92363734c9
chughgarvit57 <gavi.chugh@gmail.com>
2025-03-01 10:38:58 +0530

    Duplicate Number Program

---
+++ ProblemSolving/DuplicateNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace ProblemSolving
+{
+    internal class DuplicateNumber
+    {
+        public static void FindDuplicateNumber(int[] arr)
+        {
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    Console.WriteLine($"Duplicate number in the array is: {arr[i]}");
+                    return;
+                }
+            }
+            Console.WriteLine("No duplicate found.");
+        }
+    }
+}
