ac63c59fb4c624399392183e767d5c4055792b67
chughgarvit57 <gavi.chugh@gmail.com>
2025-03-06 12:44:18 +0530

    Stack Implementation using LinkedList

---
+++ ProblemSolving/Implementations.cs
@@ -0,0 +1,59 @@
+﻿namespace ProblemSolving
+{
+    class Implementations
+    {
+        public static void Main()
+        {
+            LinkedListStack myStack = new LinkedListStack();
+            Console.WriteLine("Press buttons for the required tasks!");
+            while (true)
+            {
+                Console.WriteLine("\n------------------ Stack Operations ----------------------\n");
+                Console.WriteLine("1 -------------------> Push Element Onto Stack");
+                Console.WriteLine("2 -------------------> Pop Element From Stack");
+                Console.WriteLine("3 -------------------> Get the Peek Element of Stack");
+                Console.WriteLine("4 -------------------> Get the size of Stack");
+                Console.WriteLine("5 -------------------> Display Stack\n");
+                int button;
+                Console.Write("Press any button: ");
+                if(!int.TryParse(Console.ReadLine(), out button))
+                {
+                    Console.WriteLine("Invalid Input! Please enter a number.");
+                    continue;
+                }
+                switch (button)
+                {
+                    case 1:
+                        Console.Write("Enter a number: ");
+                        int pusheElem = Convert.ToInt32(Console.ReadLine());
+                        myStack.Push(pusheElem);
+                        break;
+                    case 2:
+                        int popValue = myStack.Pop();
+                        Console.Write("Popped from stack: {0)" , popValue);
+                        break;
+                    case 3:
+                        myStack.Peek();
+                        break;
+                    case 4:
+                        int size = myStack.Size;
+                        Console.Write("The size is: {0}" , size);
+                        break;
+                    case 5:
+                        myStack.Display();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid button! Please press a valid button.");
+                        break;
+                }
+                Console.WriteLine("\nDo you wish to continue? (y/n)");
+                string confirmation = Console.ReadLine();
+                if(confirmation == "n")
+                {
+                    Console.WriteLine("Exiting... Have A Nice Day!");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ ProblemSolving/LinkedListStack.cs
@@ -0,0 +1,72 @@
+﻿namespace ProblemSolving
+{
+    public class LinkedListStack
+    {
+        private Node top;
+        private int size = 0;
+        public int Size { get { return size; } }
+
+        // function to check whether stack is empty
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+
+        // function to push element in the stack
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if(top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = top;
+            }
+            top = node;
+            size++;
+            Console.WriteLine($"{value} pushed onto the stack.");
+        }
+
+        // funtion to pop element from the stack
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack Underflow. Insert Values First!");
+                return -1;
+            }
+            int poppedValue = top.data;
+            top = top.next;
+            size--;
+            Console.WriteLine($"{poppedValue} popped from stack.");
+            return poppedValue;
+        }
+
+        // function to get the peek(top) element of the stack
+        public void Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack Underflow. Insert Values First!");
+                return;
+            }
+            int peekValue = top.data;
+            Console.WriteLine($"Peek element of the stack is: {peekValue}");
+        }
+
+        // function to display the stack
+        public void Display()
+        {
+            Node temp = top;
+            Console.WriteLine("Elements in the stack:");
+            while(temp != null)
+            {
+                Console.Write(temp.data + " -> ");
+                temp = temp.next;
+            }
+            Console.WriteLine("null");
+        }
+    }
+}
