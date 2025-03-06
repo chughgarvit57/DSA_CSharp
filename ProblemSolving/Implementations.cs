1aa7fce0c66962be0195c954558b8a6893d918cd
chughgarvit57 <gavi.chugh@gmail.com>
2025-03-06 14:45:13 +0530

    Queue Implementation using Linked List

---
+++ ProblemSolving/Implementations.cs
@@ -0,0 +1,62 @@
+﻿namespace ProblemSolving
+{
+    class Implementaions
+    {
+        static void Main(string[] args)
+        {
+            LinkedListQueue queue = new LinkedListQueue();
+            Console.WriteLine("Press buttons for the required tasks!");
+            while (true)
+            {
+                Console.WriteLine("\n------------------ Queue Operations ----------------------\n");
+                Console.WriteLine("1 -------------------> Insert Element into Queue");
+                Console.WriteLine("2 -------------------> Remove Element From Queue");
+                Console.WriteLine("3 -------------------> Get the Front Element of Queue");
+                Console.WriteLine("4 -------------------> Get the Rear Element of Queue");
+                Console.WriteLine("5 -------------------> Get the size of Queue");
+                Console.WriteLine("6 -------------------> Display Queue\n");
+                int button;
+                Console.Write("Press any button: ");
+                if (!int.TryParse(Console.ReadLine(), out button))
+                {
+                    Console.WriteLine("Invalid Input! Please enter a number.");
+                    continue;
+                }
+                switch (button)
+                {
+                    case 1:
+                        Console.Write("Enter a number: ");
+                        int pusheElem = Convert.ToInt32(Console.ReadLine());
+                        queue.EnQueue(pusheElem);
+                        break;
+                    case 2:
+                        queue.DeQueue();
+                        break;
+                    case 3:
+                        queue.GetFront();
+                        break;
+                    case 4:
+                        queue.GetRear();
+                        break;
+                    case 5:
+                        int size = queue.Size;
+                        Console.Write("The size is: {0}", size);
+                        break;
+                    case 6:
+                        queue.Display();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid button! Please press a valid button.");
+                        break;
+                }
+                Console.WriteLine("\nDo you wish to continue? (y/n)");
+                string confirmation = Console.ReadLine();
+                if (confirmation == "n")
+                {
+                    Console.WriteLine("Exiting... Have A Nice Day!");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ ProblemSolving/LinkedListQueue.cs
@@ -0,0 +1,87 @@
+﻿namespace ProblemSolving
+{
+    public class LinkedListQueue
+    {
+        private Node front, rear;
+        private int size = 0;
+        public int Size { get { return size; } }
+
+        // function to check whether queue is empty
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+
+        // function to add elements in the queue
+        public void EnQueue(int value)
+        {
+            Node node = new Node(value);
+            if(rear == null)
+            {
+                front = rear = node;
+                Console.WriteLine($"{value} added in the queue.");
+                size++;
+                return;
+            }
+            rear.next = node;
+            rear = node;
+            size++;
+            Console.WriteLine($"{value} added to the queue.");
+        }
+
+        // function to remove element from the queue
+        public void DeQueue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue Underflow. Insert Values first...");
+                return;
+            }
+            int removedElement = front.data;
+            front = front.next;
+            size--;
+            if(front == null)
+            {
+                rear = null;
+            }
+            Console.WriteLine("Element removed from the queue: {0}" , removedElement);
+        }
+
+        // function to get the front element in the queue
+        public void GetFront()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue Underflow. Insert values first...");
+                return;
+            }
+            int frontElement = front.data;
+            Console.WriteLine("Front element in the queue is: {0}" , frontElement);
+        }
+
+        // function to get the rear element in the queue
+        public void GetRear()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue Underflow. Insert values first...");
+                return;
+            }
+            int rearElement = rear.data;
+            Console.WriteLine("Rear element in the queue is: {0}", rearElement);
+        }
+
+        // function to display queue elements
+        public void Display()
+        {
+            Node temp = front;
+            Console.WriteLine("Elements in the queue:");
+            while(temp != null)
+            {
+                Console.Write(temp.data + " -> ");
+                temp = temp.next;
+            }
+            Console.WriteLine("null");
+        }
+    }
+}
