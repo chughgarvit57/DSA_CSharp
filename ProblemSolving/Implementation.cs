d0de8a58cdd355725f3ca61c5e584e85659040d4
chughgarvit57 <gavi.chugh@gmail.com>
2025-03-07 23:11:15 +0530

    Implemented Linear Search Program

---
+++ ProblemSolving/Implementation.cs
@@ -0,0 +1,43 @@
+﻿namespace ProblemSolving
+{
+    internal class Implementation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! Press the buttons for using different searching techniques...");
+            Console.WriteLine("\n---------------- Searching Algorithms -------------------\n");
+            while (true)
+            {
+                Console.WriteLine("1 ==================> Linear Search");
+                Console.WriteLine("2 ==================> Binary Search");
+                Console.WriteLine("3 ==================> Exit\n");
+                Console.WriteLine("Press any button:");
+                int button;
+                if(!int.TryParse(Console.ReadLine(), out button))
+                {
+                    Console.WriteLine("Invalid Input. Please enter a valid number.");
+                    continue;
+                }
+                if(button == 3)
+                {
+                    Console.WriteLine("Exiting...GoodBye!");
+                    break;
+                }
+                switch (button)
+                {
+                    case 1:
+                        Console.Write("Enter array elements separated by space: ");
+                        int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                        Console.Write("Enter the key to search: ");
+                        if (!int.TryParse(Console.ReadLine(), out int key))
+                        {
+                            Console.WriteLine("Invalid key input. Please enter a valid number.");
+                            continue;
+                        }
+                        SearchingMethods.LinearSearch(arr, key);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ProblemSolving/SearchingMethods.cs
@@ -0,0 +1,26 @@
+﻿namespace ProblemSolving
+{
+    public class SearchingMethods
+    {
+        public static void LinearSearch(int[] arr , int key)
+        {
+            bool found = false;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == key)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (found)
+            {
+                Console.WriteLine($"{key} found in the array.");
+            } 
+            else
+            {
+                Console.WriteLine($"{key} not found in the array.");
+            }
+        }
+    }
+}
